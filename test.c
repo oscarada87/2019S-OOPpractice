d9d5d931d83da3b33eb76d8090fe0869d866033e
105820006 <oscarada87@gmail.com>
2019-03-04 11:13:47 +0800

    add main function

--- test.c
+++ test.c
@@ -6,5 +6,10 @@ bool Guess(int number) {
 }
 
 int main(int argc, char const *argv[]) {
+  int guess;
+  do {
+    cout << "Choose a number between 1 - 100 :";
+    cin >> guess;
+  } while (!Guess(guess));
   return 0;
 }
