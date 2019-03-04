b5cab6988706e2eb174c0e2892a32b447abe1d52
danny0414 <hsia7f@yahoo.com.tw>
2019-03-04 11:13:43 +0800

    add main function

--- test.c
+++ test.c
@@ -1,10 +1,21 @@
+#include "stdafx.h"
+#include <string>
 #include <iostream>
-using namespace std;
-
+using namespace System;
 bool Guess(int number) {
-
+     static int target = -1;
+     if (target == -1) {
+        Random r;
+        target = r.Next() % 100 + 1;
+     }
+     if (number == target)return true;
+     return false;
 }
-
-int main(int argc, char const *argv[]) {
-  return 0;
+int main(array<System::String ^> ^args)
+{
+     int guess;
+     do {
+     std::cin >> guess;
+     } while (!Guess(guess));
+     return 0;
 }
