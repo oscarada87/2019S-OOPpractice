942c744dd54a21763500f2d0098961413aa2bbc8
danny0414 <hsia7f@yahoo.com.tw>
2019-04-24 23:46:09 +0800

    just test gamemap23

--- Source/CGameMap.h
+++ Source/CGameMap.h
@@ -34,6 +34,58 @@ namespace game_framework {
 		int map[40][40];
 		int sx, sy; // (sx, sy)為螢幕(的左上角)在地圖上的點座標
 	};
+	class CGameMap2 {
+	public:
+		CGameMap2();
+		void LoadBitmap();
+		void OnMove();
+		void OnShow();
+		void SetSX(int nx);
+		void SetSY(int ny);
+		int GetSX();
+		int GetSY();
+		int Getmapx();
+		int Getmapy();
+		int ScreenX(int x);
+		int ScreenY(int y);
+		bool monsterIsEmpty(int x, int y, CMonster *monster);
+		bool IsEmpty(int x, int y);
+	private:
+		CMovingBitmap background0;
+		CMovingBitmap background1;
+		CMovingBitmap background2;
+		CMovingBitmap background3;
+
+		int map[40][40];
+		int sx, sy; // (sx, sy)為螢幕(的左上角)在地圖上的點座標
+	};
+
+	class CGameMap3 {
+	public:
+		CGameMap3();
+		void LoadBitmap();
+		void OnMove();
+		void OnShow();
+		void SetSX(int nx);
+		void SetSY(int ny);
+		int GetSX();
+		int GetSY();
+		int Getmapx();
+		int Getmapy();
+		int ScreenX(int x);
+		int ScreenY(int y);
+		bool monsterIsEmpty(int x, int y, CMonster *monster);
+		bool IsEmpty(int x, int y);
+	private:
+		CMovingBitmap background0;
+		CMovingBitmap background1;
+		CMovingBitmap background2;
+		CMovingBitmap background3;
+
+		int map[40][40];
+		int sx, sy; // (sx, sy)為螢幕(的左上角)在地圖上的點座標
+	};
+
 }
 
 #endif
